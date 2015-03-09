f74a7e37678e90a66fcd7a465ccb9feeccd4e241
Adam Walsh <adamwalsh14@gmail.com>
2015-03-09 07:43:23 -0400

    Fixed config to deploy to gh-pages branch

--- config.rb
+++ config.rb
@@ -46,7 +46,7 @@ end
 
 activate :deploy do |deploy|
       deploy.method = :git
-      deploy.branch = 'master'
+      deploy.branch = 'gh-pages'
 end
 
 # Methods defined in the helpers block are available in templates
